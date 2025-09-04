bd5e94cfcb33b828ba710a152b6f2d73b222bcf4
David Higgins <zoul0813@me.com>
2025-09-04 12:58:32 -0400

    add: support for passing ZEAL_KERNEL_VERSION into the container for `zde kernel` compiles add: `zde kernel {config}` for building the kernel from a preset config (ie; configs/zeal8bit.default with `zde kernel zeal8bit`) refactor: `bin/activate` can now be called with `source $ZDE_PATH/bin/activate` or `eval $(zde activate)` add: bin/activate has support for the vscode extension to add Zeal env vars into VSCode env

---
+++ home/kernel.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+## Currently uses the deprecated Makefile as `config=configs/zeal8bit.default`
+## is unsupported with Cmake
+
+FULLBIN="build/os_with_romdisk.img"
+STATBYTES="stat -c %s"
+KERNEL_CONFIG="configs/${1}.default"
+shift
+
+echo "Zeal 8-bit Kernel Compiler"
+echo "Building " $KERNEL_CONFIG "for" $ZEAL_KERNEL_VERSION
+cd $ZOS_PATH
+make config=$1
+echo -e "\n"
+STATSIZE=$($STATBYTES $FULLBIN)
+
+
+mkdir -p /mnt/roms
+
+if [ "$STATSIZE" -gt 0 ]; then
+  ROM_PATH="/mnt/roms/zeal8bit-${ZEAL_KERNEL_VERSION}.img"
+  cp "$FULLBIN" "$ROM_PATH"
+  echo "Copied to $ROM_PATH"
+else
+  echo "Build failed: $FULLBIN has size 0"
+  exit 1
+fi
