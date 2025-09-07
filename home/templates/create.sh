e41bbed06851eeacd7ccf8f487a13f26b39f95d2
David Higgins <zoul0813@me.com>
2025-09-07 10:27:27 -0400

    update: zde create now has an optional `-t` flag used by the vscode extension to list only templates for parsing

--- home/templates/create.sh
+++ home/templates/create.sh
@@ -2,17 +2,28 @@
 
 SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+print_templates()
+{
+    local prefix="$1"
+    # List directories (or scripts) inside SCRIPTS_DIR
+    for dir in "$SCRIPTS_DIR"/*/ ; do
+        [ -d "$dir" ] || continue  # skip if not a directory
+        echo -e "${prefix}$(basename "$dir")"
+    done
+}
+
 if [ $# -eq 0 ]; then
     echo "# ZDE Create Project"
     echo -e "Usage: zde create <template-name> name=<project-name> [args...]\n"
     echo -e "Available Templates:\n"
 
-    # List directories (or scripts) inside SCRIPTS_DIR
-    for dir in "$SCRIPTS_DIR"/*/ ; do
-        [ -d "$dir" ] || continue  # skip if not a directory
-        echo -e "    > $(basename "$dir")"
-    done
+    print_templates "    > "
     echo ""
+    exit 0
+fi
+
+if [ $# -eq 1 ] && [ "$1" = '-t' ]; then
+    print_templates "template: "
     exit 0
 fi
 
